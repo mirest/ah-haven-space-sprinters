c803215ba419bf78602585b161f2640bb28abb3b
Okiror Frank <okirorfrank3@gmail.com>
2019-03-19 16:15:12 +0300

    chore: update readme requirements
    - ensure requirements are upto date so that they can be used for heroku deploys
    - ensure the files and folders are updated for deployment to heroku
    - Create app.json
    
    [#130635935]
    
    recover app.json
    
    ch: configures static files for heroku deploys
    
    - resolve the static files error
    [#164694212]
    
    Update settings.py
    
    chore: add static folder
    - ensure manage.py finds static folder when running collectstatic
    [#164694212]
    
    chore: configures static files for heroku deploys
    
    - resolve the static files error
    [#164694212]
    
    Update settings.py
    
    chore: add static folder
    - ensure manage.py finds static folder when running collectstatic
    [#164694212]
    
    chore: add django heroku
    - ensure django heroku is imported to run heroku settings
    [#164694212]
    
    chore: update requirements
    - ensure django heroku is installed and add to requirements file
    
    chore: setup heroku
    
    - ensure requirements are upto date so that they can be used for heroku deploys
    - ensure the files and folders are updated for deployment to heroku
    
    [#130635935]
    
    recover app.json
    
    chore: configures static files for heroku deploys
    
    - resolve the static files error
    [#164694212]
    
    Update settings.py
    
    chore: add static folder
    - ensure manage.py finds static folder when running collectstatic
    [#164694212]
    
    chore: add django heroku
    - ensure django heroku is imported to run heroku settings
    [#164694212]
    
    ch: configures static files for heroku deploys
    
    - resolve the static files error
    [#164694212]
    
    Update settings.py
    
    chore: add static folder
    - ensure manage.py finds static folder when running collectstatic
    [#164694212]
    
    chore: update requirements
    - ensure django heroku is installed and add to requirements file
    
    chore: add procfile
    - resolve error on deployed heroku app
    [#164694212]
    
    Rename Procfile.txt to Procfile
    
    chore: configure heroku app
    - ensure gunicorn is installed and update requirements file
    
    chore: resolve conflicts
    - ensure conflicts are resolved in the requirements
    [finishes 164694212]

--- authors/settings.py
+++ authors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals(), test_runner=False)
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
